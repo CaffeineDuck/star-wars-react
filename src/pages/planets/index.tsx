5cf5cf271895730c9d8ed4f8114bfe5b57b049a0
Samrid Pandit <samrid@pop-os.localdomain>
2021-02-23 19:37:17 +0545

    Added load more button in the planets

--- src/pages/planets/index.tsx
+++ src/pages/planets/index.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { useQuery } from "react-query";
-import { Heading, Text, Box, Grid, Skeleton } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useInfiniteQuery, useQuery } from "react-query";
+import { Heading, Box, Grid, Skeleton, Button, Flex } from "@chakra-ui/react";
 import { PlanetClass } from "../../utils/Types/Planets";
 import PlanetCard from "../../components/PlanetCard";
 import Error from "../../components/Error";
 import WithBackButtonLayout from "../../layouts/WithBack";
 
 const Planets = () => {
-	const fetchPlanets = async () => {
-		const res = await fetch("https://swapi.dev/api/planets/");
+	const [pageNumber, setPageNumber] = useState(1);
+
+	const fetchPlanets = async ({ pageParam = 1 }) => {
+		console.log(`https://swapi.dev/api/planets/?page=${pageParam}`);
+		const res = await fetch("https://swapi.dev/api/planets/?page=" + pageParam);
 		return res.json();
 	};
 
-	const { data, status } = useQuery("planets", fetchPlanets);
+	const {
+		data,
+		fetchNextPage,
+		hasNextPage,
+		isFetchingNextPage,
+		status,
+		isFetching,
+	} = useInfiniteQuery("planets", fetchPlanets, {
+		getNextPageParam: (lastPage, pages) => pageNumber,
+	});
+
+	const onLoadMore = (e) => {
+		setPageNumber(pageNumber + 1);
+		fetchNextPage();
+	};
+
 	const randomArray = Array.from({ length: 10 }, () =>
 		Math.floor(Math.random() * 100)
 	);
@@ -38,18 +56,40 @@ const Planets = () => {
 			{status === "error" && <Error />}
 
 			{status === "success" && (
-				<Grid templateColumns="repeat(3, 1fr)" gap={6}>
-					{data.results.map((planet: PlanetClass) => (
-						<PlanetCard key={planet.name} planet={planet} />
-					))}
-				</Grid>
-			)}
+				<div>
+					<Grid templateColumns="repeat(3, 1fr)" gap={6}>
+						{data.pages.map((group: any, index: number) => (
+							<React.Fragment key={index}>
+								{group.results.map((planet: PlanetClass, index: number) => (
+									<React.Fragment key={index}>
+										<PlanetCard planet={planet} />
+									</React.Fragment>
+								))}
+							</React.Fragment>
+						))}
+					</Grid>
 
-			{status === "idle" && <Text>Gay</Text>}
+					<Flex>
+						<Button
+						my="1rem"
+							placeSelf="center"
+							mx="auto"
+							onClick={onLoadMore}
+							disabled={!hasNextPage || isFetchingNextPage}
+						>
+							{isFetchingNextPage
+								? "Loading more..."
+								: hasNextPage
+								? "Load More"
+								: "Nothing more to load"}
+						</Button>
+					</Flex>
+				</div>
+			)}
 		</div>
 	);
 };
 
-Planets.Layout = WithBackButtonLayout
+Planets.Layout = WithBackButtonLayout;
 
 export default Planets;
